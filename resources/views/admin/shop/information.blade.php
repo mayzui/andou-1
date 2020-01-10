3b8508e7e3040b51c34e41a5b2ca7a3573b671f8
zh642033 <59350484+zh642033@users.noreply.github.com>
2020-01-10 17:32:54 +0800

    富文本框编辑器

--- resources/views/admin/shop/information.blade.php
+++ resources/views/admin/shop/information.blade.php
@@ -1,6 +1,21 @@
 @extends('admin.layouts.layout')
+@include('vendor.ueditor.assets')
 <link href="{{loadEdition('/admin/plugins/layui/css/layui.css')}}">
 <script src="{{loadEdition('/admin/plugins/layui/layui.all.js')}}"></script>
+<link rel="stylesheet" href="{{loadEdition('/assets/plugins/bootstrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{loadEdition('/assets/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{loadEdition('/assets/css/animate.css')}}">
+<link rel="stylesheet" href="{{loadEdition('/assets/css/main.css')}}">
+
+<script src="https://cdn.bootcss.com/webuploader/0.1.1/webuploader.js"></script>
+<script src="{{loadEdition('/assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{loadEdition('/assets/plugins/waypoints/waypoints.min.js')}}"></script>
+<script src="{{loadEdition('/assets/js/application.js')}}"></script>
+<script src="{{loadEdition('/assets/plugins/wizard/js/loader.min.js')}}"></script>
+<script src="{{loadEdition('/assets/plugins/wizard/js/jquery.form.js')}}"></script>
+<script src="{{loadEdition('/assets/js/modernizr-2.6.2.min.js')}}"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.bootcss.com/layer/2.3/layer.js"></script>
 @section('content')
     <div class="row">
         <div class="col-sm-12">
@@ -9,7 +24,7 @@
             </div>
             <div class="ibox-content">
                 <a class="menuid btn btn-primary btn-sm" href="javascript:history.go(-1)">返回</a>
-                
+
                 <div class="hr-line-dashed m-t-sm m-b-sm"></div>
                 <form class="form-horizontal m-t-md" action="{{ route('merchants.information') }}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
                     {!! csrf_field() !!}
@@ -80,10 +95,24 @@
                     @endif
                     <div class="form-group">
                         <label class="col-sm-2 control-label">商家简介：</label>
-                        <div class="input-group col-sm-2">
-                            <textarea cols="35" rows="9" name="desc">{{ $data -> desc or '' }}</textarea>
+                        <div class="input-group col-sm-2"  style="width: 500px">
+                            <script id="container" name="desc" type="text/plain">{!!$data->desc or ''!!}</script>
                         </div>
                     </div>
+                    <script type="text/javascript">
+                        var ue = UE.getEditor('container',{
+                            initialFrameWidth:null ,//宽度随浏览器自适应
+                            wordCount: false, //关闭字数统计
+                            elementPathEnabled : false,//隐藏元素路径
+                            autoHeightEnabled: false,//是否自动长高
+                            autoFloatEnabled: false//是否保持toolbar的位置不动
+                        });
+                        ue.ready(function() {
+                            ue.setHeight(250);
+                            ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+                        });
+                    </script>
+
                     {{--商家海报图--}}
                     <div class="form-group">
                         <label class="col-sm-2 control-label">商家海报图：</label>
