5902c0737cac92d983482d580bbc9f3d3b6e9b39
mayzui <47263068+mayzui@users.noreply.github.com>
2020-12-02 00:30:05 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,8 +64,8 @@ return [
         ],
         'oss' => [
                 'driver'        => 'oss',
-                'access_id'     => 'LTAI4FkgY2SMkBXUYHY2a3p2',
-                'access_key'    => 'LF5igYiwSIt4EDMf71O7w8mTmm14wA',
+                'access_id'     => '',
+                'access_key'    => '',
                 'bucket'        => 'andoumm',
                 'endpoint'      => 'ndoumm.oss-cn-beijing.aliyuncs.com', // OSS 外网节点或自定义外部域名
                 //'endpoint_internal' => '<internal endpoint [OSS内网节点] 如：oss-cn-shenzhen-internal.aliyuncs.com>', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
